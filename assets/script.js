1fb156b20e0c921c74ba0df05cc2be5e5a320b7f
Phil Branch <philbranch243@gmail.com>
2022-06-15 10:04:26 -0500

    now the current date display at the top of the page

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 const currentHour = moment().hours();
+const today = moment().format('MM-DD-YYYY');
 
+//console.log("Today is " + today)
+// I have successfully logged the current date, now to display it on the page
+$("#currentDay").text(today);
 
 //const currentHour = 12;  //I'm retyping this late at night and want to see that my loop is working properly.  setting 
 //the current hour to 12 will allow me to see the blocks as different colors after 5pm
 
 //loop through time blocks and apply classes based on time
 $(".time-block").each(function(){
-    console.log($(this).attr("id"))
+   // console.log($(this).attr("id"))
     const blockHour = parseInt($(this).attr("id"))
     const blockTextEl = $(this).find("textarea");
     if (blockHour === currentHour){
